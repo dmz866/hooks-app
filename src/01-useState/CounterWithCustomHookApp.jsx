312e9a906428ba39f02a3367da59bafc062d5922
David Murillo <david.murillo@ascendle.com>
2022-11-09 17:53:28 -0500

    update

---
+++ src/01-useState/CounterWithCustomHookApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const CounterWithCustomHookApp = () => {
+	return (
+		<>
+			<h1>Counter 1:</h1>
+			<hr />
+			<button className='btn btn-primary'>+</button>
+		</>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HooksApp } from './HooksApp';
+import { CounterWithCustomHookApp } from './01-useState/CounterWithCustomHookApp';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<HooksApp />
+		<CounterWithCustomHookApp />
 	</React.StrictMode>
 );
 
